8f8c3cb6d28cdb676d838ed3b132d5739378e54a
Kagsoul <160343652+Kagsoul@users.noreply.github.com>
2025-03-28 19:20:24 -0300

    Update player.py

--- script_menu/player.py
+++ script_menu/player.py
@@ -1,1 +1,15 @@
 from menu import classe_escolhida as jogador
+
+print ("para de certeza que este codigo chegou aqui: ")
+
+class jogador:
+    def __init__(self, nome, classe):
+        self.nome = nome
+        self.classe = classe
+        self.hp = classe["hp"]
+        self.dano = classe["dano"]
+        self.defesa = classe["defesa"]
+        self.passiva = classe["passiva"]
+
+    def __str__(self):
+        return f"{self.nome}, the {self.classe['nome']}"
